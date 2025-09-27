c8a8edefbbdea29082b3d087c05814514be0d1af
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-27 23:40:12 +0530

    feat: added mainscreen and mainscreen navigation

---
+++ app/src/main/java/com/example/jarprototype/navigation/MainScreenNavigation.kt
@@ -0,0 +1,20 @@
+package com.example.jarprototype.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.example.jarprototype.ui.theme.screens.MainScreen
+
+const val mainScreenRoute = "main_screen_route"
+
+fun NavController.navigateToMainScreenRoute(navOptions: NavOptions) {
+    this.navigate(mainScreenRoute, navOptions)
+}
+
+fun NavGraphBuilder.MainScreenSection(nestedScreens: NavGraphBuilder.() -> Unit) {
+    composable(route = mainScreenRoute) {
+        MainScreen()
+    }
+    nestedScreens()
+}
---
+++ app/src/main/java/com/example/jarprototype/ui/theme/screens/MainScreen.kt
@@ -0,0 +1,47 @@
+package com.example.jarprototype.ui.theme.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF713A65)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Welcome to ",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight(700),
+                fontSize = 20.sp,
+                lineHeight = 28.sp,
+                color = Color(0xFFEEEBF5),
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            )
+        )
+        Text(
+            text = "Onboarding",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight(700),
+                fontSize = 24.sp,
+                lineHeight = 32.sp,
+                color = Color(0xFFF8DC83),
+                platformStyle = PlatformTextStyle(includeFontPadding = false)
+            )
+        )
+    }
+}

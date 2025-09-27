68cd5994092f938c45ab526bc786bba8022ee3e4
visheshtiwari <visheshtiwari211@gmail.com>
2025-09-27 23:39:38 +0530

    feat: created a navhost for navigation

---
+++ app/src/main/java/com/example/jarprototype/navigation/JavNavHost.kt
@@ -0,0 +1,24 @@
+package com.example.jarprototype.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun JarNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = mainScreenRoute,
+        modifier = modifier,
+    ) {
+        MainScreenSection(
+            nestedScreens = {
+
+            }
+        )
+    }
+}
